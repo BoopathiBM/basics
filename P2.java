bf45fc0169e70b65b54c7059217053c85b881b90
boopathiBM <boopathibm19.com>
2024-12-25 09:47:05 +0530

    printf statement

--- P2.java
+++ P2.java
@@ -1,6 +1,7 @@
 
 public class P2{
-    public static void main(String[] args){
-        System.out.println(" 5*2 = 10 ");
+     public static void main(String[] args){
+        System.out.printf("%d + %d = %d", 5 ,2,10).println();
+
     }
 }
