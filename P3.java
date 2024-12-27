0ef8e301e465ececeb4171fbd8cec6dfad520c85
boopathiBM <boopathibm19.com>
2024-12-27 20:36:08 +0530

    adding methods

--- P3.java
+++ P3.java
@@ -4,25 +4,17 @@ public class P3{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         System.out.println("enter first number: ");
-        int a= scanner.nextInt();
-        System.out.println("enter 2nd number: ");
-        int b=scanner.nextInt();
-        // if(a==b){
-        //     System.out.println("both are samme");
-        // }
-        // else 
-        // System.out.println(" both are not same");
-
-        System.out.print(" enter maximum number: ");
-        int maxNum = scanner.nextInt();
-        for(int i=1;i<=maxNum;i++){
-            if(i == a){
-            System.out.println("same as value a:"+ a);
-            }
-            else
-            System.out.println(i);
-        }
-        
-
-    }
+        int a = scanner.nextInt();
+        System.out.println("enter second number: ");
+        int b = scanner.nextInt();
+        addition(a,b);
+        sub(a,b);
+       }
+       public static void addition(int i, int j){
+        System.out.println("sum of a and b is: "+ (i+j));
+       }
+       public static void sub(int k, int l){
+        System.out.println("sum of a and b is: "+ (k-l));
+       }
+    
 }
